8eef9ec5dd132943207ff1396e7c431ae6d65086
bj_liujun <bj_liujun@wisers.com>
2015-01-12 18:15:16 +0800

    update

--- src/com/yiqin/shop/action/LoginAction.java
+++ src/com/yiqin/shop/action/LoginAction.java
@@ -13,24 +13,24 @@ import com.yiqin.util.UtilKeys;
 public class LoginAction extends ActionSupport {
 
 	private static final long serialVersionUID = 316466680810245739L;
-	private String name;
-	private String password;
+	private String login_name;
+	private String login_password;
 	private UserManager userManager;
 
-	public String getName() {
-		return name;
+	public String getLogin_name() {
+		return login_name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setLogin_name(String login_name) {
+		this.login_name = login_name;
 	}
 
-	public String getPassword() {
-		return password;
+	public String getLogin_password() {
+		return login_password;
 	}
 
-	public void setPassword(String password) {
-		this.password = password;
+	public void setLogin_password(String login_password) {
+		this.login_password = login_password;
 	}
 
 	public UserManager getUserManager() {
@@ -45,13 +45,13 @@ public class LoginAction extends ActionSupport {
 		try {
 			HttpServletRequest request = ServletActionContext.getRequest();
 			// 参数判断
-			if (Util.isEmpty(name) || Util.isEmpty(password)) {
+			if (Util.isEmpty(login_name) || Util.isEmpty(login_password)) {
 				request.setAttribute("loginError", "用户名或密码不能为空");
 				return LOGIN;
 			}
 
 			// 查询信息
-			User userInfo = userManager.login(name, password);
+			User userInfo = userManager.login(login_name, login_password);
 			if (null == userInfo) {
 				request.setAttribute("loginError", "用户名或密码错误，请重新输入");
 				return LOGIN;
