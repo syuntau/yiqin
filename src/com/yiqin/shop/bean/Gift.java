70ea2c344eb5425f9e0a751c42ab067735539a9e
syuntau <syun_tau@163.com>
2015-01-18 22:25:50 +0800

    remove items

--- src/com/yiqin/shop/bean/Gift.java
+++
@@ -1,9 +0,0 @@
-package com.yiqin.shop.bean;
-
-import java.io.Serializable;
-
-public class Gift implements Serializable {
-
-	private static final long serialVersionUID = -1013549552390583975L;
-
-}
