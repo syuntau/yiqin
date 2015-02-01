98a64ba2e122fd381b475ef5581937ac4514d6e6
LiuJun <LiuJun@LiuJun-PC>
2015-02-01 17:47:31 +0800

    update dao

--- src/com/yiqin/shop/dao/IProductDao.java
+++ src/com/yiqin/shop/dao/IProductDao.java
@@ -1,5 +1,10 @@
 package com.yiqin.shop.dao;
 
+import java.util.List;
+
+import com.yiqin.shop.bean.Category;
+import com.yiqin.shop.bean.ProductBase;
+
 /**
  * 产品DAO
  * 
@@ -7,5 +12,20 @@ package com.yiqin.shop.dao;
  * 
  */
 public interface IProductDao {
+	/**
+	 * 查询所有分类菜单
+	 * 
+	 * @return 分类集
+	 */
+	public List<Category> findCategoryInfo();
+	
+	/**
+	 * 通过ID查询商品信息
+	 * 
+	 * @param pids
+	 *            商品ID 多个用逗号分隔 如（id1,id2,id3,....）
+	 * @return 对应商品
+	 */
+	public List<ProductBase> findProductInfoById(String pids);
 
 }
--- src/com/yiqin/shop/dao/impl/ProductDao.java
+++ src/com/yiqin/shop/dao/impl/ProductDao.java
@@ -1,9 +1,39 @@
 package com.yiqin.shop.dao.impl;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
+import com.yiqin.shop.bean.Category;
+import com.yiqin.shop.bean.ProductBase;
 import com.yiqin.shop.dao.IProductDao;
+import com.yiqin.util.Util;
 
 public class ProductDao extends JdbcDaoSupport implements IProductDao {
 
+	@Override
+	public List<Category> findCategoryInfo() {
+		String sql = "select * from category";
+		return getJdbcTemplate().queryForList(sql);
+	}
+
+	@Override
+	public List<ProductBase> findProductInfoById(String pids) {
+		if(Util.isNotEmpty(pids)){
+			String sql = "";
+			if(pids.contains(",")){
+				if(pids.startsWith(",")){
+					pids = pids.substring(1);
+				}else if(pids.endsWith(",")){
+					pids = pids.substring(0, pids.length()-1);
+				}
+				sql = "select * from product where productId in (?)";
+			}else{
+				sql = "select * from product where productId = ?";
+			}
+			return getJdbcTemplate().queryForList(sql, new Object[] { pids });
+		}
+		return null;
+	}
+	
 }
